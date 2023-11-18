b4fa9b551fc0dc9b4736e89c69119ffb097df12a
Paul Stier <paul-stier@gmx.de>
2023-11-18 12:41:32 +0100

    Added nunito font license

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:cash_io/db.dart';
+import 'package:cash_io/generated/assets.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   await DB.init();
+
+  // add licenses
+  LicenseRegistry.addLicense(() async* {
+    yield LicenseEntryWithLineBreaks(
+      ["Nunito"],
+      await rootBundle.loadString(Assets.licensesNunitoOFL),
+    );
+  });
+
   runApp(const MainApp());
 }
 
